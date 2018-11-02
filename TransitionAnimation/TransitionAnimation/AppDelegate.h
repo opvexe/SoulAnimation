8c5592a6311f7576eab62ef2d48a17d64eb4d8c9
FaceBook <tao_shuminX@foxmail.com>
2018-11-02 17:52:16 +0800

    no message

---
+++ TransitionAnimation/TransitionAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TransitionAnimation
+//
+//  Created by FaceBook on 2018/11/1.
+//  Copyright © 2018年 FaceBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
